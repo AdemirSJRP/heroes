03669fa2e90d2334d130ee18c83901b0c4531eee
αđ૯ʍi૨ ૨ѳđ૨igષ૯ઽ <i ig>
2016-07-25 08:38:26 -0300

    Configurado serviço para acesso a heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,84 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-export class Hero {
-    id: number;
-    name: string;
-}
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @Component({
-    selector: 'my-app',
-    styles: [`
-  .selected {
-    background-color: #CFD8DC !important;
-    color: white;
-  }
-  .heroes {
-    margin: 0 0 2em 0;
-    list-style-type: none;
-    padding: 0;
-    width: 15em;
-  }
-  .heroes li {
-    cursor: pointer;
-    position: relative;
-    left: 0;
-    background-color: #EEE;
-    margin: .5em;
-    padding: .3em 0;
-    height: 1.6em;
-    border-radius: 4px;
-  }
-  .heroes li.selected:hover {
-    background-color: #BBD8DC !important;
-    color: white;
-  }
-  .heroes li:hover {
-    color: #607D8B;
-    background-color: #DDD;
-    left: .1em;
-  }
-  .heroes .text {
-    position: relative;
-    top: -3px;
-  }
-  .heroes .badge {
-    display: inline-block;
-    font-size: small;
-    color: white;
-    padding: 0.8em 0.7em 0 0.7em;
-    background-color: #607D8B;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -4px;
-    height: 1.8em;
-    margin-right: .8em;
-    border-radius: 4px 0 0 4px;
-  }
-`],
-
-    templateUrl: 'app/app.component.html'
+  selector: 'my-app',
+  styleUrls: ['app/app.component.css'],
+  directives: [HeroDetailComponent],
+  providers: [HeroService],
+  templateUrl: 'app/app.component.html'
 })
-export class AppComponent {
-    title: string = "Tour of Heroes";
-    selectedHero: Hero;
-    heroes: Hero[] = HEROES;
+export class AppComponent implements OnInit {
 
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
+  constructor(private _heroService: HeroService) { }
+
+  title: string = "Tour of Heroes";
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  ngOnInit() {
+    console.log('carregando heroes...');
+    this._heroService.getHeroesSlowly().then((heroes) =>{
+      console.log('heroes carregados...'); 
+      this.heroes = heroes;
+    });
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
-
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
 
